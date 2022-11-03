0f7518c74293f702b4d5a61d0aaa3d5c315a0075
David Cuartas <davidcuba91@gmail.com>
2022-11-03 20:33:08 +0100

    Add .env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const port = 4001;
+const port = process.env.PORT;
 let kittyName = "Rulo";
 
 const app = express();
